8cfc388e50b08aae6cb70db1996c80d23536ebd8
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-02-03 00:16:31 +0700

    add homepage web

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kuis Kuy!</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="flex justify-center items-center flex-col bg-gray-100 h-screen">
+    <main class="h-1/2 w-3/4">
+        <h1 class="text-4xl font-black leading-10 text-center mb-16">Kuis Kuy!</h1>
+    
+        <div class="shadow-xl bg-white w-full px-8 py-8 rounded-lg">
+            <p class="text-base leading-6 font-medium text-center">Pilih peran yang kamu mainkan</p>
+            <button class="bg-slate-950 border py-2 my-2 text-white text-center rounded-md w-full">Pemain</button>
+            <button class="bg-slate-950 border py-2 my-2 text-white text-center rounded-md w-full">Guru</button>
+        </div>
+    </main>
+</body>
+</html>
