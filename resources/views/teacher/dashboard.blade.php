500cf5aa643a30c08290618fe6679c49ca0a1a46
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-02-03 01:14:38 +0700

    add dashboard page for teacher

---
+++ resources/views/teacher/dashboard.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Preparation</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body class="bg-gray-100 h-screen">
+    <nav class="bg-white w-full py-4 shadow-md">
+        <div class="flex ps-4">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                <path d="M5 7H19" stroke="#33363F" stroke-width="2" stroke-linecap="round" />
+                <path d="M5 12H19" stroke="#33363F" stroke-width="2" stroke-linecap="round" />
+                <path d="M5 17H19" stroke="#33363F" stroke-width="2" stroke-linecap="round" />
+            </svg>
+            <h1 class="text-lg leading-7 font-bold ms-4">Dashboard</h1>
+        </div>
+    </nav>
+    <main class="h-full w-full px-9 py-10">
+        <div class="w-full my-4 bg-white rounded-lg shadow-md py-4 px-4 text-sm leading-5 font-bold">
+            Buat kuis baru
+        </div>
+        <div class="w-full my-4 bg-white rounded-lg shadow-md py-4 px-4 text-sm leading-5 font-bold">
+            Daftar kuis yang dibuat
+        </div>
+    </main>
+</body>
+
+</html>
