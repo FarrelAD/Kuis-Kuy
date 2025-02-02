c8fe3bb8d4c4b6ba87eacf2ab6ed44417e7878e1
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-02-03 01:04:17 +0700

    add dynamic homepage based on user role

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,11 +10,51 @@
     <main class="h-1/2 w-3/4">
         <h1 class="text-4xl font-black leading-10 text-center mb-16">Kuis Kuy!</h1>
     
-        <div class="shadow-xl bg-white w-full px-8 py-8 rounded-lg">
+        <div id="card" class="shadow-xl bg-white w-full px-8 py-8 rounded-lg">
             <p class="text-base leading-6 font-medium text-center">Pilih peran yang kamu mainkan</p>
-            <button onclick="window.location.href='/prepare'" class="bg-slate-950 border py-2 my-2 text-white text-center rounded-md w-full">Pemain</button>
-            <button class="bg-slate-950 border py-2 my-2 text-white text-center rounded-md w-full">Guru</button>
+            <button onclick="setPlayer()" class="bg-slate-950 border py-2 my-2 text-white text-center rounded-md w-full">Pemain</button>
+            <button onclick="setTeacher()" class="bg-slate-950 border py-2 my-2 text-white text-center rounded-md w-full">Guru</button>
         </div>
     </main>
+
+
+    <template id="player-card">
+        <p class="text-base leading-6 font-medium text-center">Masukkan kode ruang bermain</p>
+        
+        <div class="grid grid-cols-4 gap-4 my-14">
+            <input type="number" name="code1" id="" placeholder="-" class="shadow-inner rounded-md bg-gray-100 w-12 h-12">
+            <input type="number" name="code1" id="" placeholder="-" class="shadow-inner rounded-md bg-gray-100 w-12 h-12">
+            <input type="number" name="code1" id="" placeholder="-" class="shadow-inner rounded-md bg-gray-100 w-12 h-12">
+            <input type="number" name="code1" id="" placeholder="-" class="shadow-inner rounded-md bg-gray-100 w-12 h-12">
+        </div>
+
+        <button onclick="window.location.href='/prepare'" class="bg-slate-950 border py-2 my-2 text-white text-center rounded-md w-full">Mainkan</button>
+    </template>
+
+
+    <template id="teacher-card">
+        <p class="text-base leading-6 font-medium text-center">Login</p>
+
+        <input type="text" name="username" id="input-username" placeholder="Nama pengguna" class="shadow-inner rounded-md bg-gray-100 w-full px-4 py-2 my-4">
+        <input type="password" name="password" id="input-password" placeholder="Password" class="shadow-inner rounded-md bg-gray-100 w-full px-4 py-2 my-4">
+
+        <button onclick="window.location.href='/dashboard'" class="bg-slate-950 border py-2 my-2 text-white text-center rounded-md w-full">Masuk</button>
+    </template>
+
+    <script>
+        const card = document.getElementById('card');
+        const playerCard = document.getElementById('player-card');
+        const teacherCard = document.getElementById('teacher-card');
+
+        function setPlayer() {
+            const playerCardTemplate = playerCard.content.cloneNode(true);
+            card.replaceChildren(playerCardTemplate);
+        }
+
+        function setTeacher() {
+            const teacherCardTemplate = teacherCard.content.cloneNode(true);
+            card.replaceChildren(teacherCardTemplate);
+        }
+    </script>
 </body>
 </html>
