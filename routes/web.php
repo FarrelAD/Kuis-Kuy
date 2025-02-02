4efa207dbee60fc7ae83a87643df82ad740f2001
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-02-03 01:14:52 +0700

    fix web routing

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+
+// Player route
 Route::get('/prepare', function () {
-    return view('student.preparation');
+    return view('player.preparation');
+});
+
+
+// Teacher route
+Route::get('/dashboard', function () {
+    return view('teacher.dashboard');
 });
