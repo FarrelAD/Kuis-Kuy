0e5c45a32de52133d0a58c9d4254dd8efab506d7
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-02-04 22:08:16 +0700

    add: new table instructor

---
+++ database/factories/InstructorFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Instructor;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Instructor>
+ */
+class InstructorFactory extends Factory
+{
+    protected $model = Instructor::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'username' => $this->faker->unique()->userName(),
+            'password' => Hash::make('password')
+        ];
+    }
+}
