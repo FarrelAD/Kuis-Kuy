2d60689e5ecfff61bb24644e764c1103a13eadcf
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-02-06 00:58:44 +0700

    update some html code and integrate with backend

--- resources/views/instructor/new-quiz.blade.php
+++ resources/views/instructor/new-quiz.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>New Quiz</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Buat Kuis</title>
     @vite('resources/css/app.css')
 </head>
 
@@ -23,41 +24,66 @@
         <section id="form-quiz-maker" class="w-full my-4 bg-white rounded-lg shadow-md py-4 px-6">
             <div>
                 <p>Nama kuis</p>
-                <input type="text" name="quiz-name" id="input-quiz-name" placeholder="Contoh: Kuis Sejarah Indonesia" class="shadow-inner rounded-md bg-gray-100 w-full px-4 py-2 my-4">
+                <input type="text" id="input-quiz-name" placeholder="Contoh: Kuis Sejarah Indonesia"
+                    class="shadow-inner rounded-md bg-gray-100 w-full px-4 py-2 my-4">
             </div>
             <div>
                 <p>Jumlah soal</p>
-                <input type="number" name="total-question" id="input-total-question" value="0" min="0" max="20" class="shadow-inner rounded-md bg-gray-100 w-full px-4 py-2 my-4">
+                <input type="number" name="total-question" id="input-total-question" value="0" min="0" max="20"
+                    class="shadow-inner rounded-md bg-gray-100 w-full px-4 py-2 my-4">
             </div>
+
+            <button id="btn-form-generator"
+                class="bg-slate-950 border py-2 my-2 text-white text-center rounded-md w-full">Buat formulir</button>
         </section>
 
         <section id="questions-container">
         </section>
+
+        <section id="btn-quiz-create-section" class="hidden">
+            <button id="btn-quiz-create"
+                class="bg-slate-950 border py-2 my-2 text-white text-center rounded-md w-full">Simpan</button>
+        </section>
     </main>
 
 
-    <template id="question-card">
-        <div class="w-full my-4 bg-white rounded-lg shadow-md py-4 px-6">
+    <template id="question-card-template">
+        <div class="question-card w-full my-4 bg-white rounded-lg shadow-md py-4 px-6">
             <h4 class="text-base leading-6 font-bold">Soal <span id="question-number">1</span></h4>
             <p>Pertanyaan</p>
-            <textarea name="question" id="input-question" class="shadow-inner rounded-md bg-gray-100 w-full px-4 py-2 my-4 resize-none" rows="4"></textarea>
+            <textarea name="question"
+                class="input-question shadow-inner rounded-md bg-gray-100 w-full px-4 py-2 my-4 resize-none"
+                rows="4"></textarea>
             <p>Jawaban</p>
             <div class="answers-container">
                 <div class="flex align-middle items-center my-4">
                     <span class="px-4 text-base leading-6 font-semibold">A</span>
-                    <input type="text" name="answer-a" id="input-answer-a" placeholder="Jawaban 1" class="shadow-inner rounded-md bg-gray-100 w-full px-4 py-2">
+                    <input type="text" placeholder="Jawaban 1"
+                        class="input-answer-a shadow-inner rounded-md bg-gray-100 w-full px-4 py-2">
                 </div>
                 <div class="flex align-middle items-center my-4">
                     <span class="px-4 text-base leading-6 font-semibold">B</span>
-                    <input type="text" name="answer-a" id="input-answer-a" placeholder="Jawaban 2" class="shadow-inner rounded-md bg-gray-100 w-full px-4 py-2">
+                    <input type="text" placeholder="Jawaban 2"
+                        class="input-answer-b shadow-inner rounded-md bg-gray-100 w-full px-4 py-2">
                 </div>
                 <div class="flex align-middle items-center my-4">
                     <span class="px-4 text-base leading-6 font-semibold">C</span>
-                    <input type="text" name="answer-a" id="input-answer-a" placeholder="Jawaban 3" class="shadow-inner rounded-md bg-gray-100 w-full px-4 py-2">
+                    <input type="text" placeholder="Jawaban 3"
+                        class="input-answer-c shadow-inner rounded-md bg-gray-100 w-full px-4 py-2">
                 </div>
                 <div class="flex align-middle items-center my-4">
                     <span class="px-4 text-base leading-6 font-semibold">D</span>
-                    <input type="text" name="answer-a" id="input-answer-a" placeholder="Jawaban 4" class="shadow-inner rounded-md bg-gray-100 w-full px-4 py-2">
+                    <input type="text" placeholder="Jawaban 4"
+                        class="input-answer-d shadow-inner rounded-md bg-gray-100 w-full px-4 py-2">
+                </div>
+                <div class="my-4">
+                    <label for="">Jawaban benar</label>
+                    <select class="correct-answer px-4 py-2 rounded-md shadow-inner bg-gray-100">
+                        <option value="a">A</option>
+                        <option value="b">B</option>
+                        <option value="c">C</option>
+                        <option value="d">D</option>
+                    </select>
                 </div>
             </div>
         </div>
@@ -65,20 +91,66 @@
 
 
     <script>
+        const CSRRF_TOKEN = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
         const inputTotalQuestion = document.getElementById('input-total-question');
         const questionsContainer = document.getElementById('questions-container');
-        const questionCardTemplate = document.getElementById('question-card');
+        const questionCardTemplate = document.getElementById('question-card-template');
+        const btnFormGenerator = document.getElementById('btn-form-generator');
+        const btnQuizCreateSection = document.getElementById('btn-quiz-create-section');
+        const btnQuizCreate = document.getElementById('btn-quiz-create');
 
-        inputTotalQuestion.addEventListener('change', function() {
+
+        btnFormGenerator.addEventListener('click', () => {
             questionsContainer.innerHTML = '';
 
-            for (let i = 0; i < this.value; i++) {
+            for (let i = 0; i < inputTotalQuestion.value; i++) {
                 const templateCloned = questionCardTemplate.content.cloneNode(true);
-                templateCloned.querySelector('#question-number').textContent = (i+1);
-                
+                templateCloned.querySelector('#question-number').textContent = (i + 1);
+
                 questionsContainer.appendChild(templateCloned);
             }
-        })
+
+            if (inputTotalQuestion.value != 0) {
+                btnQuizCreateSection.classList.remove('hidden');
+            } else {
+                btnQuizCreateSection.classList.add('hidden');
+            }
+        });
+
+        btnQuizCreate.addEventListener('click', function () {
+            const questionCards = document.querySelectorAll('.question-card');
+
+            const questionsData = Array.from(questionCards).map(item => {
+                return {
+                    question: item.querySelector('.input-question').value,
+                    answer_a: item.querySelector('.input-answer-a').value,
+                    answer_b: item.querySelector('.input-answer-b').value,
+                    answer_c: item.querySelector('.input-answer-c').value,
+                    answer_d: item.querySelector('.input-answer-d').value,
+                    correct_answer: item.querySelector('.correct-answer').value
+                }
+            });
+
+            const data = {
+                name: document.getElementById('input-quiz-name').value,
+                total_question: inputTotalQuestion.value,
+                questions: questionsData
+            }
+
+
+            fetch('/new-quiz', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRF-TOKEN': CSRRF_TOKEN
+                },
+                body: JSON.stringify(data)
+            })
+                .then(response => response.json)
+                .then(data => console.log("Response:", data))
+                .catch(error => console.error("Error:", error));
+        });
     </script>
 </body>
 
