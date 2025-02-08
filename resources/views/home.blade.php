01ad7923d86aa1b612ea5e17673b9cb12a0eda5f
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-02-08 10:23:44 +0700

    add: middleware for some protected route instructor

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kuis Kuy!</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="flex justify-center items-center flex-col bg-gray-100 h-screen">
+
+    @if (session('error'))
+        <div x-data="{ open: true }" x-init="setTimeout(() => open = false, 5000)">
+            <div x-show="open" x-transition.opacity.duration.300ms
+                class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+                <div class="bg-white rounded-lg shadow-lg p-6 w-80">
+                    <h2 class="text-lg font-bold text-red-600">Halaman web terproteksi!</h2>
+                    <p class="text-gray-700 mt-2">{{ session('error') }}</p>
+
+                    <button @click="open = false"
+                        class="mt-4 w-full bg-red-500 text-white py-2 rounded-lg hover:bg-red-600">
+                        Close
+                    </button>
+                </div>
+            </div>
+        </div>
+    @endif
+
+
     <main class="h-1/2 w-3/4">
         <h1 class="text-4xl font-black leading-10 text-center mb-16">Kuis Kuy!</h1>
 
@@ -44,14 +63,14 @@
     <template id="instructor-card">
         <form action="{{ route('login.post') }}" method="post">
             @csrf
-            
+
             <p class="text-base leading-6 font-medium text-center">Login</p>
-    
+
             <input type="text" name="username" id="input-username" placeholder="Nama pengguna" required
                 class="shadow-inner rounded-md bg-gray-100 w-full px-4 py-2 my-4">
-            <input type="password" name="password" id="input-password" placeholder="Password" required    
+            <input type="password" name="password" id="input-password" placeholder="Password" required
                 class="shadow-inner rounded-md bg-gray-100 w-full px-4 py-2 my-4">
-    
+
             <input type="submit" value="Masuk"
                 class="bg-slate-950 border py-2 my-2 text-white text-center rounded-md w-full">
             <a href="/signup" class="w-full block font-bold text-xs text-center hover:underline">Belum punya akun?</a>
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,12 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\QuizController;
+use App\Http\Middleware\AuthMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 
 // Player route
@@ -30,14 +31,17 @@ Route::post('/signup', [AuthController::class, 'signup'])
     ->name('signup.post');
 Route::post('/login', [AuthController::class, 'login'])
     ->name('login.post');
-Route::get('/dashboard', function () {
-    return view('instructor.dashboard');
-})->name('instructor.dashboard');
-Route::get('/new-quiz', [QuizController::class, 'showCreate']);
-Route::post('/new-quiz', [QuizController::class, 'create'])
-    ->name('instructor.new-quiz');
-Route::get('/all-quiz', [QuizController::class, 'showAllQuiz']);
-Route::get('/start-quiz/{id}', [QuizController::class, 'showStartQuiz'])
-    ->name('instructor.start-quiz');
-Route::post('/game-code', [QuizController::class, 'generateGameCode'])
-    ->name('instructor.get-game-code');
+
+Route::middleware([AuthMiddleware::class])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('instructor.dashboard');
+    })->name('instructor.dashboard');
+    Route::get('/new-quiz', [QuizController::class, 'showCreate']);
+    Route::post('/new-quiz', [QuizController::class, 'create'])
+        ->name('instructor.new-quiz');
+    Route::get('/all-quiz', [QuizController::class, 'showAllQuiz']);
+    Route::get('/start-quiz/{id}', [QuizController::class, 'showStartQuiz'])
+        ->name('instructor.start-quiz');
+    Route::post('/game-code', [QuizController::class, 'generateGameCode'])
+        ->name('instructor.get-game-code');
+});
